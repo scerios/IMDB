4cc6ea1c410e867134d9a7bd407c418e3588fe62
Takács Róbert <89.t.robert@gmail.com>
2019-01-16 13:59:02 +0100

    fix(method-access): methods are now package private; feat(seleniumTest): Search

--- src/main/java/Imdb.java
+++ src/main/java/Imdb.java
@@ -1,19 +1,30 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class Imdb {
+class Imdb {
   private WebDriver driver = new FirefoxDriver();
 
-  public void getUrl(String url) {
+  void getUrl(String url) {
     driver.get(url);
+    driver.manage().window().maximize();
   }
 
-  public void signIn(String email, String password) {
+  void signIn(String email, String password) {
     driver.findElement(By.className("signin-imdb-text")).click();
     driver.findElement(By.className("provider-imdb")).click();
     driver.findElement(By.id("ap_email")).sendKeys(email);
     driver.findElement(By.id("ap_password")).sendKeys(password);
     driver.findElement(By.id("signInSubmit")).click();
+  }
+
+  void search(String input, String searchValue) {
+    driver.findElement(By.id("navbar-query")).sendKeys(input);
+    WebElement quickSearchDropDown = driver.findElement(By.id("quicksearch"));
+    Select quickSearch = new Select(quickSearchDropDown);
+    quickSearch.selectByValue(searchValue);
+    driver.findElement(By.id("navbar-submit-button")).click();
   }
 }
