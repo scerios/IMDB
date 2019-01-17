22f5f51c5275d3135bd22960fa04b61fb41118e9
Takács Róbert <89.t.robert@gmail.com>
2019-01-17 16:26:59 +0100

    refactor(LogIn): raised TIMEOUT to 10s

--- src/test/java/keywords/LogIn.java
+++ src/test/java/keywords/LogIn.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LogIn {
-  public static final int WAIT_TIMEOUT = 5;
+  public static final int WAIT_TIMEOUT = 10;
   private static WebDriverWait waitDriver;
 
   private static WebElement logInUser;
