5ebd21f272e5d6a04d0ca51c2f09ef1d384e1e0e
Takács Róbert <89.t.robert@gmail.com>
2019-01-17 14:54:06 +0100

    fix(logInTest): Assert is now plain and simple

--- src/test/java/keywords/Account.java
+++ src/test/java/keywords/Account.java
@@ -7,16 +7,17 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Account {
+  public static final int WAIT_TIMEOUT = 5;
   private static WebDriverWait waitDriver;
-  private static final int WAIT_TIMEOUT = 10;
 
   private static WebElement logInUser;
   private static WebElement logInPassword;
+  private static WebElement userName;
 
   public static void logIn(WebDriver driver, String url, String email, String password) {
     System.setProperty("webdriver.gecko.driver", "C:\\Users\\Csirke\\Desktop\\Downloads\\Geckodriver\\geckodriver.exe");
-    driver.navigate().to(url);
     driver.manage().window().maximize();
+    driver.get(url);
 
     waitDriver = new WebDriverWait(driver, WAIT_TIMEOUT);
     waitDriver.until(ExpectedConditions.elementToBeClickable(By.className("signin-imdb-text")));
@@ -29,5 +30,11 @@ public class Account {
     logInUser.sendKeys(email);
     logInPassword.sendKeys(password);
     driver.findElement(By.id("signInSubmit")).click();
+  }
+
+  public static String getUserName(WebDriver driver) {
+    waitDriver = new WebDriverWait(driver, WAIT_TIMEOUT);
+    userName = waitDriver.until(ExpectedConditions.presenceOfElementLocated(By.id("nbusername")));
+    return userName.getText();
   }
 }
--- src/test/java/tests/LogInTest.java
+++ src/test/java/tests/LogInTest.java
@@ -1,36 +1,26 @@
 package tests;
 
 import keywords.Account;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
+import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LogInTest {
   private WebDriver driver;
-  private WebDriverWait waitDriver;
   private String email = "89.t.robert@gmail.com";
   private String password = "lFCTJJT0";
   private String url = "https://www.imdb.com";
-  private String userName;
 
   @Before
   public void setUp() {
     System.setProperty("webdriver.gecko.driver", "C:\\Users\\Csirke\\Desktop\\Downloads\\Geckodriver\\geckodriver.exe");
     driver = new FirefoxDriver();
-//    driver.manage().window().maximize();
   }
 
   @Test
-  public void logIn() {
+  public void logInTest() {
     Account.logIn(driver, url, email, password);
-    userName = driver.findElement(By.id("nbusername")).getText();
-    System.out.println(userName);
-    Assert.assertEquals("Róbert", userName);
+    Assert.assertEquals("Róbert", Account.getUserName(driver));
   }
 
   @After
