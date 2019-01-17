8acb8871ee6afac32a6e9e79b2496118a0487c40
Takács Róbert <89.t.robert@gmail.com>
2019-01-17 16:00:24 +0100

    feat(RecentView): added get accounts activity page

---
+++ src/test/java/keywords/RecentView.java
@@ -0,0 +1,19 @@
+package keywords;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class RecentView {
+  private static WebDriverWait waitDriver;
+  private static WebElement activityButton;
+
+  public static void getActivityPage(WebDriver driver) {
+    waitDriver = new WebDriverWait(driver, LogIn.WAIT_TIMEOUT);
+    waitDriver.until(ExpectedConditions.presenceOfElementLocated(By.id("nbusername")));
+    activityButton = driver.findElement(By.id("nbusername"));
+    activityButton.click();
+  }
+}
