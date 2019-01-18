88f35a1237740af23aab37179b0ab6549a10255f
Takács Róbert <89.t.robert@gmail.com>
2019-01-18 14:19:39 +0100

    fix(Watchlist): adding feature now works only if hasn't been added yet

--- src/test/java/keywords/WatchList.java
+++ src/test/java/keywords/WatchList.java
@@ -1,6 +1,7 @@
 package keywords;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -10,13 +11,22 @@ public class WatchList {
   private static WebDriverWait waitDriver;
 
   private static WebElement button;
+  private static WebElement added;
 
   public static void addToWatchlist(WebDriver driver) {
     waitDriver = new WebDriverWait(driver, LogIn.WAIT_TIMEOUT);
     waitDriver.until(ExpectedConditions.presenceOfElementLocated(By.className("ribbonize")));
 
+    try {
+      added = driver.findElement(By.className("wl-ribbon standalone retina inWL"));
+    } catch (NoSuchElementException e) {
+      System.out.println("There is no element like that.");
+    }
+
     button = driver.findElement(By.className("ribbonize"));
-    button.click();
+    if (added == null) {
+      button.click();
+    }
   }
 
   public static void getWatchlistPage(WebDriver driver) {
