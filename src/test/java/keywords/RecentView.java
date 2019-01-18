e4b76ebb650b71fb6a6eadfcbdd36b4caa5d0e81
Takács Róbert <89.t.robert@gmail.com>
2019-01-18 16:52:42 +0100

    feat(RecentView): added clear history function

--- src/test/java/keywords/RecentView.java
+++ src/test/java/keywords/RecentView.java
@@ -10,11 +10,19 @@ public class RecentView {
   private static WebDriverWait waitDriver;
 
   private static WebElement activityButton;
+  private static WebElement clearButton;
 
   public static void getActivityPage(WebDriver driver) {
     waitDriver = new WebDriverWait(driver, Log.WAIT_TIMEOUT);
     waitDriver.until(ExpectedConditions.presenceOfElementLocated(By.id("nbusername")));
     activityButton = driver.findElement(By.id("nbusername"));
     activityButton.click();
+  }
+
+  public static void clearHistory(WebDriver driver) {
+    waitDriver = new WebDriverWait(driver, Log.WAIT_TIMEOUT);
+    waitDriver.until(ExpectedConditions.presenceOfElementLocated(By.id("clear_rvi")));
+    clearButton = driver.findElement(By.id("clear_rvi"));
+    clearButton.click();
   }
 }
