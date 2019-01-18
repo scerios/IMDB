6dfd317b4153a8fb66d737261bcfa5f6ccc6d8bc
Takács Róbert <89.t.robert@gmail.com>
2019-01-18 12:32:38 +0100

    fix(WatchList): add to watchlist now functional

--- src/test/java/keywords/WatchList.java
+++ src/test/java/keywords/WatchList.java
@@ -5,31 +5,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.List;
 
 public class WatchList {
   private static WebDriverWait waitDriver;
 
-  private static WebElement quickSearchDropDown;
-  private static WebElement watchListMenu;
   private static WebElement button;
-  private static List<WebElement> searchResult;
 
-  public static void addToWatchlist(WebDriver driver, String searchInput) {
+  public static void addToWatchlist(WebDriver driver) {
     waitDriver = new WebDriverWait(driver, LogIn.WAIT_TIMEOUT);
-    waitDriver.until(ExpectedConditions.presenceOfElementLocated(By.id("navWatchlistMenu")));
+    waitDriver.until(ExpectedConditions.presenceOfElementLocated(By.className("ribbonize")));
 
-    watchListMenu = driver.findElement(By.id("navWatchlistMenu"));
-    watchListMenu.click();
-    waitDriver.until(ExpectedConditions.presenceOfElementLocated(By.className("button-title")));
-
-    button = driver.findElement(By.className("button-title"));
+    button = driver.findElement(By.className("ribbonize"));
     button.click();
-    waitDriver.until(ExpectedConditions.presenceOfElementLocated(By.id("add-to-list-search")));
-
-    quickSearchDropDown = driver.findElement(By.id("add-to-list-search"));
-    quickSearchDropDown.sendKeys(searchInput);
-    searchResult = driver.findElements(By.xpath("//div[@id='add-to-list-search-results']//a[@class='search_item']"));
-    System.out.println(searchResult.size());
   }
 }
